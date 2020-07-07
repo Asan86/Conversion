94ac182e9a36cdcb9591b2ddb0dbc49cabb45350
Turdukulov Asan <asanturdukulov86@gmail.com>
2020-07-07 14:26:54 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+int myInt = 9;
+double myDouble = myInt;
+double myDoubl1 = 3.98;
+int myInt1 = (int) myDoubl1;
+        System.out.println(myInt);
+        System.out.println(myDouble);
+        System.out.println(myInt1);
+    }
+}
